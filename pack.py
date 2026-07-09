f9e673321d2e053b53d71f702afd3049fd760078
Drime-A202 <2422661600@qq.com>
2026-07-09 15:25:33 +0800

    fix: 重新打包包含所有模块并添加 base href

--- pack.py
+++ pack.py
@@ -1,3 +1,11 @@
 from stlitepack import pack
 
-pack(app_file="app.py")
+pack(
+    app_file="app.py",
+    extra_files=[
+        "config.py",
+        "weather_api.py",
+        "term_mapping.py",
+        "canvas_builder.py"
+    ]
+)
